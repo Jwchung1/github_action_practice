02194a5f80bbfd7c164e30c8e2c752aec175671c
Jwchung1 <137181670+Jwchung1@users.noreply.github.com>
2023-11-13 21:41:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ def fib(n:int) -> int:
   return n if n<2 else fib(n-1)+fib(n-2)
 
 def test_fibonacci():
-  assert fib(int(os.environ["FIB_INPUT])) == 55
+  assert fib(int(os.environ["FIB_INPUT"])) == 55
